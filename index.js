3b3e186a2a44c2d38c84f3e1b32d07fa17d10014
AL <112848500+DevyatovAlexy@users.noreply.github.com>
2024-02-06 21:32:42 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
+const ipBlackList = process.env.IP_BLACK_LIST.split(',')
 
-const localIP ='172.128.5.2';
-const ipBlackLsit = process.env.IP_BLACK_LIST
-.split(',');
-function getIpBlackList (local){
-    let num = 0;
-    while (num < ipBlackLsit.length){
-        if (local[num] === localIP){
-            console.log(local[num] + ' в черном списке')
-            break;
+function checkIpBlackList(ip) {
+    for (let i = 0; i < ipBlackList.length; i++) {
+        if (ipBlackList[i] === ip) {
+            return ipBlackList[i]
         }
-        num++;
     }
 }
-getIpBlackList(ipBlackLsit)
+
+console.log(checkIpBlackList('172.128.5.2'))
