b5e77f573af01340c12fc695ea33f15921fc8676
AL <112848500+DevyatovAlexy@users.noreply.github.com>
2024-02-07 17:02:32 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const ipBlackList = process.env.IP_BLACK_LIST.split(',')
+const ipBlackList = process.env.IP_BLACK_LIST.split(',').map(item => item.trim()).filter(removingEmptyLine => removingEmptyLine != '')
+// removingEmptyLine - удоляет пустую строку
+const checkIpBlackList = (ip) => {
 
-function checkIpBlackList(ip) {
     for (let i = 0; i < ipBlackList.length; i++) {
         if (ipBlackList[i] === ip) {
-            return ipBlackList[i]
+            return false
+
         }
+
     }
 }
 
 console.log(checkIpBlackList('172.128.5.2'))
+
+try {
+    if (ipBlackList.length <= 0) {
+        throw new Error('IP_BLACK_LIST is not set')
+    }
+} catch (error) {
+
+    console.log('Сообщение об ошибке: ' + error);
+}
