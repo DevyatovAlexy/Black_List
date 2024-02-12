04d10854d5298e3b26ccf1ce9bc474bdb05a197e
AL <112848500+DevyatovAlexy@users.noreply.github.com>
2024-02-12 10:56:43 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,18 @@ const getBlackList = () => {
         const ipBlackList = process.env.IP_BLACK_LIST.split(',').map(item => item.trim()).filter(item => item != '')
         if (ipBlackList.length <= 0) {
             throw new Error('IP_BLACK_LIST is not set')
-        } return ipBlackList
+        }
+        return ipBlackList
     } catch (error) {
         console.log('Сообщение об ошибке: ' + error);
     }
 }
 
-// Проверяет наличе ip в ipBlackList 
+// Проверяет наличе ip в ipBlackList
 function checkIpBlackList(ip) {
- let blackList = getBlackList().find(elem => elem === ip )
-  return blackList === ip
+
+    return getBlackList().find(item => item === ip) != null
 }
 
 console.log(checkIpBlackList('172.128.5.2'))
+
